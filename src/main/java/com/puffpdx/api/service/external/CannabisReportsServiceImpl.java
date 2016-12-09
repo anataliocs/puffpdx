ddd28e37209d9cae61098029b67dc0966c0d9d11
chris.anatalio <anataliocs@gmail.com>
2016-12-08 20:47:17 -0800

    Checkmark buttons and rest call

--- src/main/java/com/puffpdx/api/service/external/CannabisReportsServiceImpl.java
+++ src/main/java/com/puffpdx/api/service/external/CannabisReportsServiceImpl.java
@@ -5,10 +5,13 @@ import com.puffpdx.api.config.WiremockResponses;
 import com.puffpdx.api.model.strains.Effects;
 import com.puffpdx.api.model.strains.Strains;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class CannabisReportsServiceImpl implements CannabisReportsService {
@@ -23,6 +26,9 @@ public class CannabisReportsServiceImpl implements CannabisReportsService {
     @Override
     public Strains getStrains(String sort, String page) {
         Strains strains = null;
+
+        //TODO Implement wiremocking
+        //strains = restTemplate.getForObject("https://www.cannabisreports.com/api/v1.0/strains?sort=name&page=3", Strains.class);
 
         try {
             strains = objectMapper.readValue(WiremockResponses.strainsResponseBody, Strains.class);
